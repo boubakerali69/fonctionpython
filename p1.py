b87dba40197257c66a407d01bed62be0083141f3
boubakerali69 <boubaker.ali69@gmail.com>
2020-04-19 19:11:53 +0100

    creation fonction somme

--- p1.py
+++ p1.py
@@ -1,5 +1,7 @@
 print("mon premier pogramme")
+def somme(a,b):
+    return a+b
 a=int(input("donner la valeur de a="))
 b=int(input("donner la valeur de b ="))
-print(f"soomme de {a} + {b}={a+b}")
+print(f"soomme de {a} + {b}={somme(a,b)}")
 
